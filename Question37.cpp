e9d3ab04ca807ed40d9aad5b8f1d1df5d44fef7a
anuj rathore <ajrathore1800@gmail.com>
2025-03-11 17:13:59 +0530

    Day15

---
+++ Question37.cpp
@@ -0,0 +1,16 @@
+// // Game - truth and dare 
+// #include<iostream>
+// using std::cout;
+// using std::cin;
+
+// int main(){
+//     cout<<u8"←";
+// }
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "← Left Arrow" << std::endl;  // Unicode Left Arrow
+    return 0;
+}
